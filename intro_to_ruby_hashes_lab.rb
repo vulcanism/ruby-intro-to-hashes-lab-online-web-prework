364c26adf61e4c7dd487638d868795764d0e45aa
Savannah C <sgcorrell@gmail.com>
2019-03-09 21:58:52 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -74,10 +74,10 @@ def monopoly_with_fourth_tier
 	        "mortgage_value" => "$200"
 	      },
 	      :b_and_o_railroad => {
-	        "mortage_value" => "$400"
+	        "mortgage_value" => "$400"
 	      },
 	      :shortline => {
-	        "mortage_value" => "$800"
+	        "mortgage_value" => "$800"
 	      }
 	      
 	    },
